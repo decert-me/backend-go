cf0691207a439033edc1a68885f3e25d5542c630
梁文杰 <1139629972@qq.com>
2023-03-31 10:54:25 +0800

    add: recommend

--- internal/app/service/transaction.go
+++ internal/app/service/transaction.go
@@ -2,12 +2,14 @@ package service
 
 import (
 	"backend-go/internal/app/model"
+	"backend-go/internal/app/model/request"
 	"backend-go/pkg/log"
+	"github.com/tidwall/gjson"
 	"go.uber.org/zap"
 )
 
-func (s *Service) HashSubmit(address string, hash string) (err error) {
-	transHash := model.Transaction{SendAddr: address, Hash: hash}
+func (s *Service) HashSubmit(address string, req request.HashSubmitRequest) (err error) {
+	transHash := model.Transaction{SendAddr: address, Hash: req.Hash, Params: []byte(gjson.Parse(req.Params).Raw)}
 	// save
 	if err = s.dao.CreateTransaction(&transHash); err != nil {
 		log.Errorv("CreateTransaction error", zap.Error(err))
