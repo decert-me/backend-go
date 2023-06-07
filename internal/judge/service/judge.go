571d63eefe9ebd6549de82e79a82a8a9aafd4be8
梁文杰 <1139629972@qq.com>
2023-06-07 16:26:50 +0800

    add: 添加 Python 支持

--- internal/judge/service/judge.go
+++ internal/judge/service/judge.go
@@ -3,6 +3,7 @@ package service
 import (
 	"backend-go/internal/judge/model/request"
 	"backend-go/internal/judge/model/response"
+	"errors"
 )
 
 func (s *Service) TryRun(address string, req request.TryRunReq) (tryRunRes response.TryRunRes, err error) {
@@ -12,6 +13,12 @@ func (s *Service) TryRun(address string, req request.TryRunReq) (tryRunRes respo
 		return s.JavaScriptTryRun(req)
 	} else if req.Lang == "Golang" {
 		return s.GolangTryRun(req)
+	} else if req.Lang == "Python" {
+		return s.PythonTryRun(req)
+	} else if req.Lang == "TypeScript" {
+		return s.TypeScriptTryRun(req)
+	} else {
+		return tryRunRes, errors.New("暂不支持的语言")
 	}
 	return
 }
@@ -23,6 +30,12 @@ func (s *Service) TryTestRun(address string, req request.TryTestRunReq) (tryRunR
 		return s.JavaScriptTryTestRun(req)
 	} else if req.Lang == "Golang" {
 		return s.GolangTryTestRun(req)
+	} else if req.Lang == "Python" {
+		return s.PythonTryTestRun(req)
+	} else if req.Lang == "TypeScript" {
+		return s.TypeScriptTryTestRun(req)
+	} else {
+		return tryRunRes, errors.New("暂不支持的语言")
 	}
 	return
 }
