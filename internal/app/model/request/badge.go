6def50141931ee8c7e83f0e828a5fa40ff63febb
梁文杰 <1139629972@qq.com>
2023-06-30 14:15:41 +0800

    add: 添加判断题目修改提醒功能

--- internal/app/model/request/badge.go
+++ internal/app/model/request/badge.go
@@ -1,15 +1,17 @@
 package request
 
 type PermitClaimBadgeReq struct {
-	TokenId int64  `json:"tokenId" binding:"required"`
-	Score   int64  `json:"score"`
-	Answer  string `json:"answer" binding:"required"`
+	TokenId        int64  `json:"tokenId" binding:"required"`
+	Score          int64  `json:"score"`
+	Answer         string `json:"answer" binding:"required"`
+	StandardAnswer string `json:"standard_answer"`
 }
 type SubmitClaimTweetReq struct {
-	TokenId  int64  `json:"tokenId"`
-	TweetUrl string `json:"tweetUrl"`
-	Score    int64  `json:"score"  binding:"required"`
-	Answer   string `json:"answer" binding:"required"`
+	TokenId        int64  `json:"tokenId"`
+	TweetUrl       string `json:"tweetUrl"`
+	Score          int64  `json:"score"  binding:"required"`
+	Answer         string `json:"answer" binding:"required"`
+	StandardAnswer string `json:"standard_answer"`
 }
 
 type UpdateBadgeURIRequest struct {
