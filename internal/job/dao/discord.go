8aef869d5649535584d82563d79296f54f565f73
梁文杰 <1139629972@qq.com>
2023-07-03 09:54:24 +0800

    add: 成功空投添加@用户

--- internal/job/dao/discord.go
+++ internal/job/dao/discord.go
@@ -1,14 +1,23 @@
 package dao
 
 import (
+	"backend-go/internal/app/model"
 	"backend-go/pkg/log"
 	"fmt"
 	"github.com/bwmarrin/discordgo"
+	"github.com/tidwall/gjson"
 	"go.uber.org/zap"
 	"time"
 )
 
 func (d *Dao) AirdropSuccessNotice(address string, tokenID int64) {
+	// 查询用户信息
+	var user model.Users
+	err := d.db.Model(&model.Users{}).Select("socials").Where("address = ?", address).First(&user).Error
+	if err != nil {
+		log.Errorv("查询用户失败", zap.String("address", address), zap.Error(err))
+		return
+	}
 	Token := d.c.Discord.Token
 	// Create a new Discord session using the provided bot token.
 	dg, err := discordgo.New("Bot " + Token)
@@ -20,7 +29,6 @@ func (d *Dao) AirdropSuccessNotice(address string, tokenID int64) {
 	defer dg.Close()
 	// In this example, we only care about receiving message events.
 	dg.Identify.Intents = discordgo.IntentsGuildMessages
-
 	// Open a websocket connection to Discord and begin listening.
 	err = dg.Open()
 	if err != nil {
@@ -29,8 +37,8 @@ func (d *Dao) AirdropSuccessNotice(address string, tokenID int64) {
 	}
 	// 拼接信息
 	link := fmt.Sprintf("https://polygonscan.com/token/%s?a=%s", d.c.Contract.Badge, tokenID)
-	description := fmt.Sprintf("Address: %s \n TokenID: %d \n %s", address, tokenID, link)
-	footer := discordgo.MessageEmbedFooter{Text: "web3-bot"}
+	description := fmt.Sprintf("Address: %s \n TokenID: %d \n %s \n <@&%s>", address, tokenID, link, gjson.Get(string(user.Socials), "discord.id"))
+	footer := discordgo.MessageEmbedFooter{Text: "decert-bot"}
 	embeds := []*discordgo.MessageEmbed{
 		{Color: 0x0099FF, Title: "SBT 空投",
 			Description: description,
@@ -67,7 +75,7 @@ func (d *Dao) AirdropFailNotice(address string, tokenID int64, reason string) {
 	}
 	// 拼接信息
 	description := fmt.Sprintf("Address: %s \n TokenID: %d \n Reason: %s", address, tokenID, reason)
-	footer := discordgo.MessageEmbedFooter{Text: "web3-bot"}
+	footer := discordgo.MessageEmbedFooter{Text: "decert-bot"}
 	embeds := []*discordgo.MessageEmbed{
 		{Color: 0x0099FF, Title: "SBT 空投失败",
 			Description: description,
