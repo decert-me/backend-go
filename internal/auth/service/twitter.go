a4fdfb551c237f33839286ea62642bca4e86d864
梁文杰 <1139629972@qq.com>
2023-07-06 11:32:00 +0800

    en: 获取用户时间线推文

--- internal/auth/service/twitter.go
+++ internal/auth/service/twitter.go
@@ -2,19 +2,17 @@ package service
 
 import (
 	"backend-go/internal/app/model"
-	"backend-go/internal/auth/model/receive"
 	"backend-go/internal/auth/model/request"
 	"backend-go/internal/auth/utils"
 	"backend-go/pkg/log"
-	"encoding/json"
 	"errors"
 	"fmt"
 	twitterClient "github.com/dghubble/go-twitter/twitter"
 	"github.com/dghubble/oauth1"
 	"github.com/dghubble/oauth1/twitter"
-	"github.com/imroc/req/v3"
+	"github.com/tidwall/gjson"
 	"go.uber.org/zap"
-	"time"
+	"strconv"
 )
 
 // TwitterAuthorizationURL 推特获取登陆链接
@@ -68,8 +66,6 @@ func (s *Service) TwitterCallback(address string, req request.TwitterCallbackReq
 		log.Errorv("List error", zap.Error(err))
 		return
 	}
-	fmt.Println(list)
-	return
 	// 查找是否已经绑定过
 	binding, err := s.dao.TwitterIsBinding(user.ID)
 	if err != nil {
@@ -79,7 +75,7 @@ func (s *Service) TwitterCallback(address string, req request.TwitterCallbackReq
 		return result, errors.New("AlreadyBinding")
 	}
 	// 绑定到用户
-	if err = s.dao.TwitterBinding(address, user.ID, user.Name); err != nil {
+	if err = s.dao.TwitterBinding(address, user.ID, user.Name, accessToken, accessSecret); err != nil {
 		return result, err
 	}
 	return
@@ -88,35 +84,32 @@ func (s *Service) TwitterCallback(address string, req request.TwitterCallbackReq
 // TwitterUserTweet 查找关于Decert的推特
 func (s *Service) TwitterUserTweet(claimReq request.TwitterClaimReq) (err error) {
 	// 查找用户推特信息
-	twitterID, err := s.dao.TwitterQueryIdByAddress(claimReq.Address)
-	if err != nil || twitterID == "" {
+	twitterData, err := s.dao.TwitterQueryByAddress(claimReq.Address)
+	if err != nil || twitterData == "" {
 		return errors.New("RecordNotFound")
 	}
 	// 查询用户推特列表
-	client := req.C().
-		SetTimeout(30 * time.Second).
-		SetCommonRetryCount(2).
-		SetUserAgent("v2TweetLookupJS").
-		SetCommonBearerAuthToken(s.c.Auth.Twitter.BearerToken)
-	url := fmt.Sprintf("https://api.twitter.com/2/users/%s/tweets", twitterID)
-	req, err := client.R().Get(url)
-	fmt.Println(req.String())
-	var tweets receive.TweetsGenerated
-	if err = json.Unmarshal(req.Bytes(), &tweets); err != nil {
-		log.Errorv("json.Unmarshal error", zap.Error(err))
-		return err
+	accessToken := gjson.Get(twitterData, "accessToken").String()
+	accessSecret := gjson.Get(twitterData, "accessSecret").String()
+	token := oauth1.NewToken(accessToken, accessSecret)
+	oaConfig := oauth1.NewConfig(s.c.Auth.Twitter.ConsumerKey, s.c.Auth.Twitter.ConsumerSecret)
+	httpClient := oaConfig.Client(oauth1.NoContext, token)
+	client := twitterClient.NewClient(httpClient)
+	userID := gjson.Get(twitterData, "id").Int()
+	list, _, err := client.Timelines.UserTimeline(&twitterClient.UserTimelineParams{UserID: userID, Count: 50})
+	if err != nil {
+		log.Errorv("List error", zap.Error(err))
+		return
 	}
-	fmt.Println(tweets.Data)
-	return
 	// 匹配推文
-	for _, v := range tweets.Data {
-		if utils.CheckIfMatchClaimTweet(twitterID, v.Text) {
+	for _, v := range list {
+		if utils.CheckIfMatchClaimTweet(strconv.Itoa(int(claimReq.TokenId)), v.Text) {
 			// 写入待空投列表
 			s.dao.TwitterCreateTweetClaim(&model.ClaimBadgeTweet{
 				Address: claimReq.Address,
 				TokenId: claimReq.TokenId,
 				Score:   claimReq.Score,
-				TweetId: v.ID,
+				TweetId: strconv.Itoa(int(v.ID)),
 			})
 		}
 	}
