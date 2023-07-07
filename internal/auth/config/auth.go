8c3e8b70770ffcc69d402ded4238b71492384baa
梁文杰 <1139629972@qq.com>
2023-07-07 09:57:53 +0800

    del: 删除无用配置

--- internal/auth/config/auth.go
+++ internal/auth/config/auth.go
@@ -6,7 +6,6 @@ type Twitter struct {
 	ConsumerKey    string `mapstructure:"consumer-key" json:"consumer-key" yaml:"consumer-key"`
 	ConsumerSecret string `mapstructure:"consumer-secret" json:"consumer-secret" yaml:"consumer-secret"`
 	CallbackURL    string `mapstructure:"callback-url" json:"callback-url" yaml:"callback-url"`
-	BearerToken    string `mapstructure:"bearer-token" json:"bearer-token" yaml:"bearer-token"`
 }
 
 type Github struct {
