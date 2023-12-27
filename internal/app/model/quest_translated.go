83e4cbd4521b13e6ccda30b13573488b69bb8f3d
梁文杰 <1139629972@qq.com>
2023-12-27 11:49:10 +0800

    en: 更新model

--- internal/app/model/quest_translated.go
+++ internal/app/model/quest_translated.go
@@ -9,6 +9,7 @@ type QuestTranslated struct {
 	gorm.Model
 	TokenId     int64          `gorm:"column:token_id;not null;index:language_token_id,unique" json:"tokenId"`
 	Title       string         `gorm:"column:title;comment:标题;type:varchar" json:"title" form:"title"` // 标题
+	Answer      string         `gorm:"column:answer" json:"answer"`                                    // 答案
 	Description string         `gorm:"column:description;type:varchar" json:"description" form:"description"`
 	MetaData    datatypes.JSON `gorm:"column:meta_data" json:"metadata"`    // 元数据
 	QuestData   datatypes.JSON `gorm:"column:quest_data" json:"quest_data"` // 元数据
