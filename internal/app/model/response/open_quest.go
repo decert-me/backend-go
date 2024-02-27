63b898ae863bc84a3245908cc8e1137dc96d5689
梁文杰 <1139629972@qq.com>
2024-02-27 10:28:16 +0800

    fix: 修复开放题列表报错

--- internal/app/model/response/open_quest.go
+++ internal/app/model/response/open_quest.go
@@ -8,7 +8,7 @@ import (
 type UserOpenQuestJsonElements struct {
 	ID                    uint           `gorm:"primarykey"`
 	Address               string         `gorm:"column:address;type:varchar(44);comment:钱包地址;index:address_tokenId" json:"address" form:"address"`
-	TokenId               int64          `gorm:"column:token_id;index:address_tokenId" json:"token_id"`
+	TokenId               string         `gorm:"column:token_id;index:address_tokenId" json:"token_id"`
 	OpenQuestReviewStatus uint8          `gorm:"column:open_quest_review_status;default:0;comment:评阅开放题状态 1 未审核 2 已审核" json:"open_quest_review_status" form:"open_quest_review_status"` // // 评阅开放题状态 1 未审核 2 已审核
 	OpenQuestReviewTime   string         `gorm:"column:open_quest_review_time;comment:评阅开放题时间" json:"open_quest_review_time" form:"open_quest_review_time"`
 	UpdatedAt             time.Time      `gorm:"column:updated_at" json:"updated_at"`
