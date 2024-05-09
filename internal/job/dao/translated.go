dd409318ea1d36e67f8e4c29c90a590ee5b58f2b
liangjies <1139629972@qq.com>
2024-05-09 16:06:35 +0800

    fix: 修改挑战清除翻译

--- internal/job/dao/translated.go
+++ internal/job/dao/translated.go
@@ -5,6 +5,7 @@ import "backend-go/internal/app/model"
 func (d *Dao) DeleteQuestTranslated(tokenId string) (err error) {
 	err = d.db.Model(&model.QuestTranslated{}).
 		Where("token_id = ?", tokenId).
+		Unscoped().
 		Delete(&model.QuestTranslated{}).Error
 	return
 }
