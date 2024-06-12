b9055e8689375d3f82ab62e281a64c1c5ce754c8
liangjies <1139629972@qq.com>
2024-06-12 10:21:29 +0800

    add: 开放题用户标签

---
+++ internal/app/dao/tag.go
@@ -0,0 +1,29 @@
+package dao
+
+import "backend-go/internal/app/model"
+
+func (d *Dao) GetUserTags(userID uint) (tags []string, err error) {
+	err = d.db.Model(&model.UsersTag{}).Select("tag.name").
+		Joins("join tag on users_tag.tag_id = tag.id").
+		Where("users_tag.user_id = ?", userID).
+		Find(&tags).Error
+	return
+}
+
+func (d *Dao) GetUserNameTagsByAddress(address string) (name string, tags []string, err error) {
+	var user model.Users
+	err = d.db.Model(&model.Users{}).
+		Where("address = ?", address).
+		First(&user).Error
+	if err != nil {
+		return
+	}
+	if user.Name != nil {
+		name = *user.Name
+	}
+	err = d.db.Model(&model.UsersTag{}).Select("tag.name").
+		Joins("join tag on users_tag.tag_id = tag.id").
+		Where("users_tag.user_id = ?", user.ID).
+		Find(&tags).Error
+	return
+}
